5af192331dee80f1d6ee1acb223179fd56ece535
Luka Mijatovic <luka.mijatovic@fielmann.com>
2023-12-12 16:24:03 +0100

    modified:   ios/Flutter/Debug.xcconfig 	modified:   ios/Flutter/Release.xcconfig 	modified:   ios/Runner.xcodeproj/project.pbxproj 	modified:   ios/Runner.xcworkspace/contents.xcworkspacedata 	modified:   lib/main.dart 	modified:   macos/Flutter/Flutter-Debug.xcconfig 	modified:   macos/Flutter/Flutter-Release.xcconfig 	modified:   macos/Runner.xcodeproj/project.pbxproj 	modified:   macos/Runner.xcworkspace/contents.xcworkspacedata 	modified:   pubspec.lock 	modified:   pubspec.yaml 	ios/Podfile 	ios/Podfile.lock 	lib/login.dart 	macos/Podfile 	macos/Podfile.lock

--- lib/main.dart
+++ lib/main.dart
@@ -1,49 +1,100 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
+import 'login.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (context) => MyAppState(),
-      child: MaterialApp(
-        title: 'Namer App',
-        theme: ThemeData(
-          useMaterial3: true,
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
-        ),
-        home: MyHomePage(),
-      ),
-    );
-  }
-}
-
-class MyAppState extends ChangeNotifier {
-  
-}
-
-class MyHomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    var appState = context.watch<MyAppState>();
-
-    return Scaffold(
-      body: Column(
-          children: [
-            Align(
-              alignment: Alignment.center,
-              child: Text(
-                'Dashie',
-                textAlign: TextAlign.center,
+    return MaterialApp(
+      title: 'Flutter layout demo',
+      home: Builder(
+        builder: (context) => Scaffold(
+          body: Stack(
+            children: [
+              Container(
+                color: Colors.white,
               ),
-            ),
-          ],
+              Positioned(
+                bottom: 75,
+                left: 20,
+                child: GestureDetector(
+                  child: Container(
+                    height: 40,
+                    width: 165,
+                    decoration: BoxDecoration(
+                      color: const Color.fromARGB(255, 45, 44, 44),
+                      borderRadius: BorderRadius.circular(40),
+                    ),
+                    child: Center(
+                      child: Text(
+                        "Register",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Positioned(
+                bottom: 75,
+                left: 200,
+                child: GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => LoginScreen()),
+                    );
+                  },
+                  child: Container(
+                    height: 40,
+                    width: 165,
+                    decoration: BoxDecoration(
+                      color: Color.fromARGB(255, 217, 217, 217),
+                      borderRadius: BorderRadius.circular(40),
+                    ),
+                    child: Center(
+                      child: Text(
+                        "Log In",
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Center(
+                child: Text(
+                  "Dashie",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 40,
+                  ),
+                ),
+              ),
+              Positioned(
+                bottom: 355,
+                left: 155,
+                child: Text(
+                "Your Daily Driver.",
+                style: TextStyle(
+                  fontSize: 10,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                ),
+              ),
+              
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
